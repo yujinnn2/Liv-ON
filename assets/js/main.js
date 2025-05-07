cea8571a7a179885dd513f711f7f7f2c380ef99f
yujinnn2 <yujinee111@gmail.com>
2025-05-07 14:50:00 +0900

    o

---
+++ assets/js/main.js
@@ -0,0 +1,63 @@
+
+
+let swiper = new Swiper(".sec02_mySwiper", {
+  slidesPerView: 5,
+  spaceBetween: 26,
+  centeredSlides: true,
+  grabCursor: true,
+  pagination: {
+    el: ".sec02_swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".sec02_swiper-button-next",
+    prevEl: ".sec02_swiper-button-prev",
+  }
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const swiper = new Swiper('.swiper', {
+    // direction: 'vertical',
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    grabCursor: true,
+    speed: 1000,
+    mousewheel: true,
+    effect: 'slide',
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    on: {
+      init: function () {
+        // 초기화 시 첫 슬라이드 애니메이션 트리거
+        this.slides[this.activeIndex].classList.add('swiper-slide-active');
+      },
+      slideChange: function () {
+        // 슬라이드 변경 시 애니메이션 리셋
+        const slides = this.slides;
+        for (let i = 0; i < slides.length; i++) {
+          slides[i].classList.remove('swiper-slide-active');
+        }
+        slides[this.activeIndex].classList.add('swiper-slide-active');
+      }
+    },
+    // 마지막 swiper에서 하단으로 스크롤 가능띠 
+    on: {
+      reachEnd: function () {
+        swiper.mousewheel.disable();
+      }
+    }
+  });
+  window.addEventListener('wheel', function (event) {
+    if (event.deltaY < 0) {
+      swiper.mousewheel.enable();
+    } else if (event.deltaY > 0) {
+    }
+  });
+});
