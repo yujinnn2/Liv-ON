33b962c761c2afa95eebbf9110724360b1482122
yujinnn2 <yujinee111@gmail.com>
2025-05-09 18:29:07 +0900

    dddd

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,59 +1,66 @@
-// swiper 02
-let swiper = new Swiper(".sec02_mySwiper", {
-  slidesPerView: 4,
-  spaceBetween: 30,
-  centeredSlides: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".next_btn",
-  },
-});
 
+    // // HERO SLIDER
+    // var menu = [];
+    // jQuery('.swiper-slide').each( function(index){
+    //     menu.push( jQuery(this).find('.slide-inner').attr("data-text") );
+    // });
+    // var interleaveOffset = 0.5;
+    // var swiperOptions = {
+    //     loop: true,
+    //     speed: 1000,
+    //     parallax: true,
+    //     autoplay: {
+    //         delay: 6500,
+    //         disableOnInteraction: false,
+    //     },
+    //     watchSlidesProgress: true,
+    //     pagination: {
+    //         el: '.swiper-pagination',
+    //         clickable: true,
+    //     },
 
-//swiper 01
-document.addEventListener('DOMContentLoaded', function () {
-  const swiper = new Swiper('.sec01_swiper', {
-    direction: 'vertical',
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    grabCursor: true,
-    speed: 1000,
-    mousewheel: true,
-    effect: 'slide',
-    autoplay: {
-      delay: 4000,
-      disableOnInteraction: false,
-    },
-    on: {
-      init: function () {
-        // 초기화 시 첫 슬라이드 애니메이션 트리거
-        this.slides[this.activeIndex].classList.add('.sec01_swiper-slide-active');
-      },
-      slideChange: function () {
-        // 슬라이드 변경 시 애니메이션 리셋
-        const slides = this.slides;
-        for (let i = 0; i < slides.length; i++) {
-          slides[i].classList.remove('swiper-slide-active');
-        }
-        slides[this.activeIndex].classList.add('swiper-slide-active');
-      }
-    },
-    // 마지막 swiper에서 하단으로 스크롤 가능띠 
-    on: {
-      reachEnd: function () {
-        swiper.mousewheel.disable();
-      }
-    }
-  });
-  window.addEventListener('wheel', function (event) {
-    if (event.deltaY < 0) {
-      swiper.mousewheel.enable();
-    } else if (event.deltaY > 0) {
-    }
-  });
-});
+    //     navigation: {
+    //         nextEl: '.swiper-button-next',
+    //         prevEl: '.swiper-button-prev',
+    //     },
 
+    //     on: {
+    //         progress: function() {
+    //             var swiper = this;
+    //             for (var i = 0; i < swiper.slides.length; i++) {
+    //                 var slideProgress = swiper.slides[i].progress;
+    //                 var innerOffset = swiper.width * interleaveOffset;
+    //                 var innerTranslate = slideProgress * innerOffset;
+    //                 swiper.slides[i].querySelector(".slide-inner").style.transform =
+    //                 "translate3d(" + innerTranslate + "px, 0, 0)";
+    //             }      
+    //         },
+
+    //         touchStart: function() {
+    //           var swiper = this;
+    //           for (var i = 0; i < swiper.slides.length; i++) {
+    //             swiper.slides[i].style.transition = "";
+    //           }
+    //         },
+
+    //         setTransition: function(speed) {
+    //             var swiper = this;
+    //             for (var i = 0; i < swiper.slides.length; i++) {
+    //                 swiper.slides[i].style.transition = speed + "ms";
+    //                 swiper.slides[i].querySelector(".slide-inner").style.transition =
+    //                 speed + "ms";
+    //             }
+    //         }
+    //     }
+    // };
+
+    // var swiper = new Swiper(".swiper-container", swiperOptions);
+
+    // // DATA BACKGROUND IMAGE
+    // var sliderBgSetting = $(".slide-bg-image");
+    // sliderBgSetting.each(function(indx){
+    //     if ($(this).attr("data-background")){
+    //         $(this).css("background-image", "url(" + $(this).data("background") + ")");
+    //     }
+    // });
 
