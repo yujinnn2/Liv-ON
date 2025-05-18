7a9a1c3e624887e48541414409f6024870902661
yujinnn2 <yujinee111@gmail.com>
2025-05-19 02:31:48 +0900

    d

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,27 +8,36 @@ triggerTabList.forEach(function (triggerEl) {
   })
 })
 
-  AOS.init();
+AOS.init();
 
 
 
-   document.addEventListener('DOMContentLoaded', () => {
-        const textBlocks = document.querySelectorAll('.text-block');
-        let currentIndex = 0;
+document.addEventListener('DOMContentLoaded', () => {
+  const textBlocks = document.querySelectorAll('.text-block');
+  let currentIndex = 0;
 
-        // 스크롤 이벤트 감지
-        window.addEventListener('wheel', (e) => {
-            e.preventDefault(); // 기본 스크롤 방지 (선택적)
+  // 스크롤 이벤트 감지
+  window.addEventListener('wheel', (e) => {
+    e.preventDefault(); // 기본 스크롤 방지 (선택적)
 
-            // 스크롤 방향에 따라 인덱스 변경
-            if (e.deltaY > 0) {
-                currentIndex = Math.min(currentIndex + 1, textBlocks.length - 1);
-            } else {
-                currentIndex = Math.max(currentIndex - 1, 0);
-            }
+    // 스크롤 방향에 따라 인덱스 변경
+    if (e.deltaY > 0) {
+      currentIndex = Math.min(currentIndex + 1, textBlocks.length - 1);
+    } else {
+      currentIndex = Math.max(currentIndex - 1, 0);
+    }
 
-            // 모든 텍스트 비활성화 후 현재 텍스트만 활성화
-            textBlocks.forEach(block => block.classList.remove('active'));
-            textBlocks[currentIndex].classList.add('active');
-        });
-    });
+    // 모든 텍스트 비활성화 후 현재 텍스트만 활성화
+    textBlocks.forEach(block => block.classList.remove('active'));
+    textBlocks[currentIndex].classList.add('active');
+  });
+});
+
+
+// hbg button 
+let hbg = document.querySelector('.hbg');
+hbg.onclick = function () {
+  navBar = document.querySelector('.nav-bar');
+  navBar.classList.toggle('active');
+  $(function () { });
+};
